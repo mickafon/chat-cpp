d30d39af91bf47eb1b4ab6b4efa56db037045eca
CXIR <ludwig.demonceau@gmail.com>
2018-02-07 15:33:37 +0100

    Initial Commit
    Initial Commit

---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -2
+
+int main(){
+
+    SOCKET sock;
+
+    return EXIT_SUCCESS;
+}
